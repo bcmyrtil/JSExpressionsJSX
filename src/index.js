1e7c5e6e217a86c7e5fbe2f1e5d86d832a30ac00
bcmyrtil <85637220+bcmyrtil@users.noreply.github.com>
2022-02-13 15:16:35 -0500

    Add JS Expressions in html in JSX file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Breana";
+const lName = "Myrtil";
+const favNo = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    <p>Your favorite number is {favNo}</p>
+  </div>,
+  document.getElementById("root")
+);
